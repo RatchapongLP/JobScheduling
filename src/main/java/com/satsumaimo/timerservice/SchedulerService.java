e7950ebaa340efdd050e6d11857cfa98431a5429
Ratchapong Lertpun <ratchapong.lpz@gmail.com>
2024-04-25 18:09:36 +0700

    added schedule method and getAllRunningTimers method

--- src/main/java/com/satsumaimo/timerservice/SchedulerService.java
+++ src/main/java/com/satsumaimo/timerservice/SchedulerService.java
@@ -1,13 +1,19 @@
 package com.satsumaimo.timerservice;
 
+import com.satsumaimo.info.TimerInfo;
+import com.satsumaimo.util.TimerUtils;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
+import org.quartz.*;
+import org.quartz.impl.matchers.GroupMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class SchedulerService {
@@ -18,6 +24,38 @@ public class SchedulerService {
     @Autowired
     public SchedulerService(Scheduler scheduler) {
         this.scheduler = scheduler;
+    }
+
+    public <T extends Job> void schedule(final Class<T> jobClass, final TimerInfo timerInfo) {
+
+        final JobDetail jobDetail = TimerUtils.buildJobDetail(jobClass, timerInfo);
+        final Trigger trigger = TimerUtils.buildTrigger(jobClass, timerInfo);
+
+        try {
+            scheduler.scheduleJob(jobDetail, trigger);
+        } catch (SchedulerException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+
+    public List<TimerInfo> getAllRunningTimers() {
+
+        try {
+            return scheduler.getJobKeys(GroupMatcher.anyGroup())
+                    .stream()
+                    .map(jobKey -> {
+                        try {
+                            JobDetail jobDetail = scheduler.getJobDetail(jobKey);
+                            return (TimerInfo) jobDetail.getJobDataMap().get(jobKey.getName());
+                        } catch (SchedulerException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+        } catch (SchedulerException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @PostConstruct
