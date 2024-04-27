b5d19936aad83c0f5ffbc643688173879565a066
Ratchapong Lertpun <ratchapong.lpz@gmail.com>
2024-04-27 16:21:23 +0700

    edited the execute method

--- src/main/java/com/satsumaimo/jobs/GreetingJob.java
+++ src/main/java/com/satsumaimo/jobs/GreetingJob.java
@@ -1,8 +1,9 @@
 package com.satsumaimo.jobs;
 
+import com.satsumaimo.info.TimerInfo;
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,7 +14,11 @@ public class GreetingJob implements Job {
     private static final Logger LOGGER = LoggerFactory.getLogger(GreetingJob.class);
 
     @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
+    public void execute(JobExecutionContext context) {
+
+        JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
+        TimerInfo timerInfo = (TimerInfo) jobDataMap.get(GreetingJob.class.getSimpleName());
         LOGGER.info("How do you do?");
+        LOGGER.info("Remaining fire count = '{}'", timerInfo.getRemainingFireCount());
     }
 }
